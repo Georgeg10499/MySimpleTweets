7319a42f4d3a9bd411a589ad91f94b024bece98f
Jorge Garcia Ruiz <jorge10gr@fb.com>
2018-07-03 14:50:37 -0700

    User can see profile pictures, usernames and body texts

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,26 +2,46 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
 
     TwitterClient client;
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+        client = TwitterApp.getRestClient(this);
 
-         client = TwitterApp.getRestClient(this);
-         populateTimeline();
+        //find the RecyclerView
+        rvTweets = (RecyclerView) findViewById(R.id.rvTweet);
+        //init the arraylist (datasource)
+        tweets = new ArrayList<>();
+        //construct the adapter from this dataSource
+        tweetAdapter = new TweetAdapter(tweets);
+        //RecyclerView setup (layout manager, use adapter)
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        //set the adapter
+        rvTweets.setAdapter(tweetAdapter);
+
+        populateTimeline();
     }
 
     private void populateTimeline() {
@@ -33,7 +53,21 @@ public class TimelineActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("TwitterClient", response.toString());
+                //Log.d("TwitterClient", response.toString());
+                //iterate trough JSONArray
+                //for each entry, deserialize the JSON object
+                for (int i = 0; i < response.length(); i++) {
+                    //convert each object to Tweet model
+                    //add that tweet model to our data source
+                    //notify the adapter that we've added an item
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(tweets.size() - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
